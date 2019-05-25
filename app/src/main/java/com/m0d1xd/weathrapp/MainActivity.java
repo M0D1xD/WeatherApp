f44ff015501cbbabf1b25d58baf9174edc65306f
Mohammad Zakout <modi.zak.4@gmail.com>
2019-05-25 15:02:19 +0400

    MainActivity Nav view init

--- app/src/main/java/com/m0d1xd/weathrapp/MainActivity.java
+++ app/src/main/java/com/m0d1xd/weathrapp/MainActivity.java
@@ -1,14 +1,32 @@
 package com.m0d1xd.weathrapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            initViews();
+        }
     }
+
+    private void initViews() {
+        bottomNavigationView = findViewById(R.id.bottom_nav_view);
+        navController = Navigation.findNavController(this, R.id.fragment);
+
+    }
+
+
 }
