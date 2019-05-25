bbec9c03b38fedbe70151983667335696e196145
Mohammad Zakout <modi.zak.4@gmail.com>
2019-05-25 18:24:34 +0400

    Class for Constants

---
+++ app/src/main/java/com/m0d1xd/weathrapp/util/Constants.java
@@ -0,0 +1,9 @@
+package com.m0d1xd.weathrapp.util;
+
+public class Constants {
+    public static final String CITY_LIST = "city_list";
+    public static final String LAST_COORD_LAT = "lat";
+    public static final String LAST_COORD_LON = "lon";
+    public static final String FRAGMENT_MAP_BUNDLE = "map_bundle";
+
+}
