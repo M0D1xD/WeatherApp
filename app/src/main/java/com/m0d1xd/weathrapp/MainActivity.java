e65e215868b1c0aa95fe2bd238e477e8fc658bd3
Mohammad Zakout <modi.zak.4@gmail.com>
2019-05-25 15:13:44 +0400

    Weather Api Request

--- app/src/main/java/com/m0d1xd/weathrapp/MainActivity.java
+++ app/src/main/java/com/m0d1xd/weathrapp/MainActivity.java
@@ -12,18 +12,34 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.m0d1xd.weathrapp.Remote.RetrofitInstance;
+import com.m0d1xd.weathrapp.Remote.WeatherService;
+import com.m0d1xd.weathrapp.model.WeatherApi.City;
+import com.m0d1xd.weathrapp.model.WeatherApi.WeatherResponse;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_LOCATION = 1;
+    private static final int CITIES_COUNT = 20;
     private LocationManager locationManager;
 
     public static Double latitude, longitude;
 
     private BottomNavigationView bottomNavigationView;
     private NavController navController;
+
+    private Call<WeatherResponse> call;
+    public static WeatherService mServices;
+    public static List<City> Cities;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
     private void initViews() {
         bottomNavigationView = findViewById(R.id.bottom_nav_view);
         navController = Navigation.findNavController(this, R.id.fragment);
-
+        mServices = RetrofitInstance.getRetrofitInstance();
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
@@ -81,6 +97,31 @@ public class MainActivity extends AppCompatActivity {
                 latitude = location2.getLatitude();
                 longitude = location2.getLongitude();
             }
+        }
+        if (longitude != null && latitude != null) {
+            call = mServices.getWeather(getString(R.string.weather_api),
+                    String.valueOf(latitude),
+                    String.valueOf(longitude),
+                    CITIES_COUNT);
+            call.enqueue(new Callback<WeatherResponse>() {
+                @Override
+                public void onResponse(Call<WeatherResponse> call, Response<WeatherResponse> response) {
+
+                    if (response.isSuccessful() && response.body() != null) {
+                        if (response.body().getCode().equals("200")) {
+                            Cities = response.body().getList();
+                        } else {
+                            Log.d(TAG, "onResponse: " + response.body().getCode());
+                            Log.d(TAG, "onResponse: " + response.body().getMessage());
+                        }
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<WeatherResponse> call, Throwable t) {
+                    Log.d(TAG, "onFailure: " + t.getMessage());
+                }
+            });
         }
     }
 
