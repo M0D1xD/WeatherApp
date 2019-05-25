d63241c7b5dac5db0cdf1231a2e78ffd137603c7
Mohammad Zakout <modi.zak.4@gmail.com>
2019-05-25 14:49:42 +0400

    Map Fragment init

--- app/src/main/java/com/m0d1xd/weathrapp/ui/fragments/MapFragment.java
+++ app/src/main/java/com/m0d1xd/weathrapp/ui/fragments/MapFragment.java
@@ -3,20 +3,28 @@ package com.m0d1xd.weathrapp.ui.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.Marker;
 import com.m0d1xd.weathrapp.R;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback,
+        GoogleMap.OnInfoWindowClickListener, GoogleMap.InfoWindowAdapter {
 
+    private static final String TAG = "MapFragment";
+    private MapView mMapView;
+    private GoogleMap mGoogleMap;
 
     public MapFragment() {
         // Required empty public constructor
@@ -27,7 +35,43 @@ public class MapFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+        return view;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mMapView = view.findViewById(R.id.map);
+        if (mMapView != null) {
+            mMapView.onCreate(null);
+            mMapView.onResume();
+            mMapView.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public View getInfoWindow(Marker marker) {
+        return null;
+    }
+
+    @Override
+    public View getInfoContents(Marker marker) {
+        return null;
+    }
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getView().getContext());
+        mGoogleMap = googleMap;
+        mGoogleMap.setOnInfoWindowClickListener(this);
+        mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mGoogleMap.setOnInfoWindowClickListener(this);
+        mGoogleMap.setInfoWindowAdapter(this);
+    }
 }
