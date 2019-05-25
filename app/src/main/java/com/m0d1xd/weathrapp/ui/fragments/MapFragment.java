597109e2e3f17f90c2245545d1e6123a654738b9
Mohammad Zakout <modi.zak.4@gmail.com>
2019-05-25 18:24:01 +0400

    MapFragment Marker Setup Improvments

--- app/src/main/java/com/m0d1xd/weathrapp/ui/fragments/MapFragment.java
+++ app/src/main/java/com/m0d1xd/weathrapp/ui/fragments/MapFragment.java
@@ -86,61 +86,71 @@ public class MapFragment extends Fragment implements OnMapReadyCallback,
     }
 
     private void setUpMarkers(final GoogleMap mGoogleMap) {
-        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission
-                (getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            //Default Value for Rostov
-            MainActivity.latitude = 47.2357;
-            MainActivity.longitude = 39.7015;
-
-        } else {
-            Location location = MainActivity.locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-            Location location1 = MainActivity.locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            Location location2 = MainActivity.locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
-
-            if (location != null) {
-                MainActivity.latitude = location.getLatitude();
-                MainActivity.longitude = location.getLongitude();
-
-            } else if (location1 != null) {
-                MainActivity.latitude = location1.getLatitude();
-                MainActivity.longitude = location1.getLongitude();
-
-            } else if (location2 != null) {
-                MainActivity.latitude = location2.getLatitude();
-                MainActivity.longitude = location2.getLongitude();
+        if (MainActivity.Cities != null) {
+            mGoogleMap.clear();
+            for (City city : MainActivity.Cities) {
+                LatLng Coords = new LatLng(city.getCoord().getLat(), city.getCoord().getLon());
+                mGoogleMap.addMarker(new MarkerOptions().position(Coords).title(city.getName())).setTag(city);
+                mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(Coords));
+                mGoogleMap.moveCamera(CameraUpdateFactory.zoomTo(10));
             }
-        }
-        if (MainActivity.longitude != null && MainActivity.latitude != null) {
-            call = MainActivity.mServices.getWeather(getString(R.string.weather_api),
-                    String.valueOf(MainActivity.latitude),
-                    String.valueOf(MainActivity.longitude),
-                    CITIES_COUNT);
-            call.enqueue(new Callback<WeatherResponse>() {
-                @Override
-                public void onResponse(Call<WeatherResponse> call, Response<WeatherResponse> response) {
-                    if (response.isSuccessful()) {
-                        MainActivity.Cities = response.body().getList();
-                        mGoogleMap.clear();
-                        if (MainActivity.Cities != null) {
-                            for (City city : MainActivity.Cities) {
-                                LatLng Coords = new LatLng(city.getCoord().getLat(), city.getCoord().getLon());
-                                mGoogleMap.addMarker(new MarkerOptions().position(Coords).title(city.getName())).setTag(city);
-                                mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(Coords));
-                                mGoogleMap.moveCamera(CameraUpdateFactory.zoomTo(10));
-                            }
-                        }
-                    } else {
-                        Log.d(TAG, "onResponse: " + response.body().getCode());
-                        Log.d(TAG, "onResponse: " + response.body().getMessage());
-                    }
-                }
+        } else {
+            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission
+                    (getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                //Default Value for Rostov
+                MainActivity.latitude = 47.2357;
+                MainActivity.longitude = 39.7015;
 
-                @Override
-                public void onFailure(Call<WeatherResponse> call, Throwable t) {
-                    Log.d(TAG, "onFailure: " + t.getMessage());
+            } else {
+                Location location = MainActivity.locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                Location location1 = MainActivity.locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                Location location2 = MainActivity.locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+
+                if (location != null) {
+                    MainActivity.latitude = location.getLatitude();
+                    MainActivity.longitude = location.getLongitude();
+
+                } else if (location1 != null) {
+                    MainActivity.latitude = location1.getLatitude();
+                    MainActivity.longitude = location1.getLongitude();
+
+                } else if (location2 != null) {
+                    MainActivity.latitude = location2.getLatitude();
+                    MainActivity.longitude = location2.getLongitude();
                 }
-            });
+            }
+            if (MainActivity.longitude != null && MainActivity.latitude != null) {
+                call = MainActivity.mServices.getWeather(getString(R.string.weather_api),
+                        String.valueOf(MainActivity.latitude),
+                        String.valueOf(MainActivity.longitude),
+                        CITIES_COUNT);
+                call.enqueue(new Callback<WeatherResponse>() {
+                    @Override
+                    public void onResponse(Call<WeatherResponse> call, Response<WeatherResponse> response) {
+                        if (response.isSuccessful()) {
+                            MainActivity.Cities = (ArrayList<City>) response.body().getList();
+                            mGoogleMap.clear();
+                            if (MainActivity.Cities != null) {
+                                for (City city : MainActivity.Cities) {
+                                    LatLng Coords = new LatLng(city.getCoord().getLat(), city.getCoord().getLon());
+                                    mGoogleMap.addMarker(new MarkerOptions().position(Coords).title(city.getName())).setTag(city);
+                                    mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(Coords));
+                                    mGoogleMap.moveCamera(CameraUpdateFactory.zoomTo(10));
+                                }
+                            }
+                        } else {
+                            Log.d(TAG, "onResponse: " + response.body().getCode());
+                            Log.d(TAG, "onResponse: " + response.body().getMessage());
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<WeatherResponse> call, Throwable t) {
+                        Log.d(TAG, "onFailure: " + t.getMessage());
+                    }
+                });
+            }
         }
 
     }
