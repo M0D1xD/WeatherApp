1877a73fb7984ef65beb37b4a748ee4a638a2163
Mohammad Zakout <modi.zak.4@gmail.com>
2019-05-25 15:03:42 +0400

    MainActivity Location Request

--- app/src/main/java/com/m0d1xd/weathrapp/MainActivity.java
+++ app/src/main/java/com/m0d1xd/weathrapp/MainActivity.java
@@ -1,14 +1,24 @@
 package com.m0d1xd.weathrapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_LOCATION = 1;
+    private LocationManager locationManager;
+
+    public static Double latitude, longitude;
 
     private BottomNavigationView bottomNavigationView;
     private NavController navController;
@@ -25,7 +35,35 @@ public class MainActivity extends AppCompatActivity {
     private void initViews() {
         bottomNavigationView = findViewById(R.id.bottom_nav_view);
         navController = Navigation.findNavController(this, R.id.fragment);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                REQUEST_LOCATION);
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission
+                (MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //Default Value for Rostov
+            latitude = 47.2357;
+            longitude = 39.7015;
 
+        } else {
+            Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            Location location1 = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            Location location2 = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+
+            if (location != null) {
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+
+            } else if (location1 != null) {
+                latitude = location1.getLatitude();
+                longitude = location1.getLongitude();
+
+            } else if (location2 != null) {
+                latitude = location2.getLatitude();
+                longitude = location2.getLongitude();
+            }
+        }
     }
 
 
