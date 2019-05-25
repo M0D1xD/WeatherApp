37df8f4e97b5bcc0151d9d19e015a61a4c671d15
Mohammad Zakout <modi.zak.4@gmail.com>
2019-05-25 16:22:19 +0400

    Relocating API Request

--- app/src/main/java/com/m0d1xd/weathrapp/MainActivity.java
+++ app/src/main/java/com/m0d1xd/weathrapp/MainActivity.java
@@ -19,18 +19,14 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.m0d1xd.weathrapp.Remote.RetrofitInstance;
 import com.m0d1xd.weathrapp.Remote.WeatherService;
 import com.m0d1xd.weathrapp.model.WeatherApi.City;
-import com.m0d1xd.weathrapp.model.WeatherApi.WeatherResponse;
 
 import java.util.List;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static LocationManager locationManager;
     private static final int REQUEST_LOCATION = 1;
-    private static final int CITIES_COUNT = 20;
-    private LocationManager locationManager;
     private static final String TAG = "MainActivity";
 
     public static Double latitude, longitude;
@@ -38,7 +34,6 @@ public class MainActivity extends AppCompatActivity {
     private BottomNavigationView bottomNavigationView;
     private NavController navController;
 
-    private Call<WeatherResponse> call;
     public static WeatherService mServices;
     public static List<City> Cities;
 
@@ -55,6 +50,9 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigationView = findViewById(R.id.bottom_nav_view);
         navController = Navigation.findNavController(this, R.id.fragment);
         mServices = RetrofitInstance.getRetrofitInstance();
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                REQUEST_LOCATION);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
@@ -70,62 +68,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        ActivityCompat.requestPermissions(MainActivity.this,
-                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                REQUEST_LOCATION);
-        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission
-                (MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            //Default Value for Rostov
-            latitude = 47.2357;
-            longitude = 39.7015;
 
-        } else {
-            Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-            Location location1 = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            Location location2 = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
-
-            if (location != null) {
-                latitude = location.getLatitude();
-                longitude = location.getLongitude();
-
-            } else if (location1 != null) {
-                latitude = location1.getLatitude();
-                longitude = location1.getLongitude();
-
-            } else if (location2 != null) {
-                latitude = location2.getLatitude();
-                longitude = location2.getLongitude();
-            }
-        }
-        if (longitude != null && latitude != null) {
-            call = mServices.getWeather(getString(R.string.weather_api),
-                    String.valueOf(latitude),
-                    String.valueOf(longitude),
-                    CITIES_COUNT);
-            call.enqueue(new Callback<WeatherResponse>() {
-                @Override
-                public void onResponse(Call<WeatherResponse> call, Response<WeatherResponse> response) {
-
-                    if (response.isSuccessful()) {
-                        if (response.body().getCode().equals("200")) {
-                            Cities = response.body().getList();
-                        } else {
-                            Log.d(TAG, "onResponse: " + response.body().getCode());
-                        }
-                    } else {
-                        Log.d(TAG, "onResponse: " + response.body().getCode());
-                        Log.d(TAG, "onResponse: " + response.body().getMessage());
-                    }
-                }
-
-                @Override
-                public void onFailure(Call<WeatherResponse> call, Throwable t) {
-                    Log.d(TAG, "onFailure: " + t.getMessage());
-                }
-            });
-        }
     }
 
 
--- app/src/main/java/com/m0d1xd/weathrapp/ui/fragments/MapFragment.java
+++ app/src/main/java/com/m0d1xd/weathrapp/ui/fragments/MapFragment.java
@@ -1,12 +1,19 @@
 package com.m0d1xd.weathrapp.ui.fragments;
 
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,6 +29,11 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.m0d1xd.weathrapp.MainActivity;
 import com.m0d1xd.weathrapp.R;
 import com.m0d1xd.weathrapp.model.WeatherApi.City;
+import com.m0d1xd.weathrapp.model.WeatherApi.WeatherResponse;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class MapFragment extends Fragment implements OnMapReadyCallback,
@@ -30,6 +42,10 @@ public class MapFragment extends Fragment implements OnMapReadyCallback,
     private static final String TAG = "MapFragment";
     private MapView mMapView;
     private GoogleMap mGoogleMap;
+
+    private Call<WeatherResponse> call;
+
+    private static final int CITIES_COUNT = 20;
 
     public MapFragment() {
         // Required empty public constructor
@@ -58,20 +74,66 @@ public class MapFragment extends Fragment implements OnMapReadyCallback,
 
     }
 
-    private void setUpMarkers(GoogleMap mGoogleMap) {
-        mGoogleMap.clear();
-        LatLng point = new LatLng(MainActivity.latitude, MainActivity.longitude);
-        mGoogleMap.addMarker(new MarkerOptions().position(point).title("Rostov On Don"));
-        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(point));
-        mGoogleMap.moveCamera(CameraUpdateFactory.zoomTo(10));
-        if (MainActivity.Cities != null) {
-            for (City city : MainActivity.Cities) {
-                LatLng Coords = new LatLng(city.getCoord().getLat(), city.getCoord().getLon());
-                mGoogleMap.addMarker(new MarkerOptions().position(Coords).title(city.getName())).setTag(city);
-                mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(Coords));
-                mGoogleMap.moveCamera(CameraUpdateFactory.zoomTo(10));
+    private void setUpMarkers(final GoogleMap mGoogleMap) {
+
+
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission
+                (getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //Default Value for Rostov
+            MainActivity.latitude = 47.2357;
+            MainActivity.longitude = 39.7015;
+
+        } else {
+            Location location = MainActivity.locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            Location location1 = MainActivity.locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            Location location2 = MainActivity.locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+
+            if (location != null) {
+                MainActivity.latitude = location.getLatitude();
+                MainActivity.longitude = location.getLongitude();
+
+            } else if (location1 != null) {
+                MainActivity.latitude = location1.getLatitude();
+                MainActivity.longitude = location1.getLongitude();
+
+            } else if (location2 != null) {
+                MainActivity.latitude = location2.getLatitude();
+                MainActivity.longitude = location2.getLongitude();
             }
         }
+        if (MainActivity.longitude != null && MainActivity.latitude != null) {
+            call = MainActivity.mServices.getWeather(getString(R.string.weather_api),
+                    String.valueOf(MainActivity.latitude),
+                    String.valueOf(MainActivity.longitude),
+                    CITIES_COUNT);
+            call.enqueue(new Callback<WeatherResponse>() {
+                @Override
+                public void onResponse(Call<WeatherResponse> call, Response<WeatherResponse> response) {
+                    if (response.isSuccessful()) {
+                        MainActivity.Cities = response.body().getList();
+                        mGoogleMap.clear();
+                        if (MainActivity.Cities != null) {
+                            for (City city : MainActivity.Cities) {
+                                LatLng Coords = new LatLng(city.getCoord().getLat(), city.getCoord().getLon());
+                                mGoogleMap.addMarker(new MarkerOptions().position(Coords).title(city.getName())).setTag(city);
+                                mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(Coords));
+                                mGoogleMap.moveCamera(CameraUpdateFactory.zoomTo(10));
+                            }
+                        }
+                    } else {
+                        Log.d(TAG, "onResponse: " + response.body().getCode());
+                        Log.d(TAG, "onResponse: " + response.body().getMessage());
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<WeatherResponse> call, Throwable t) {
+                    Log.d(TAG, "onFailure: " + t.getMessage());
+                }
+            });
+        }
+
     }
 
     @Override
